a85f5c7c0a750bc97b10c6fac1fd9e53d15522d8
BlackGear <Daniel65536@gmail.com>
2014-08-29 22:59:31 +0800

    New param 'b' clear.

--- ingrex_auth.py
+++ ingrex_auth.py
@@ -16,7 +16,7 @@ class Auth(object):
     
     def fetch_v(self):
         """
-        Fetch the version of the ingress api.
+        Fetch param v of the ingress api.
         """
         headers = {
         'referer': 'https://www.ingress.com/intel',
@@ -24,7 +24,30 @@ class Auth(object):
             ' (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'),
         }
         request = requests.get('https://www.ingress.com/jsc/gen_dashboard.js', headers=headers, verify=False)
-        reg = r'"([\da-f]{40})"'
+        reg = '"([\da-f]{40})"'
         v = re.search(reg, request.text).group(1)
         self.config['Verify']['v'] = v
+    
+    def fetch_b(self):
+        """
+        Fetch param b of the ingress api.
+        """
+        headers = {
+        'referer': 'https://www.ingress.com/intel',
+        'user-agent': ('Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36'
+            ' (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'),
+        }
+        cookies = {
+        'SACSID': self.config['Token']['sacsid'],
+        'csrftoken': self.config['Token']['csrftoken'],
+        'GOOGAPPUID': self.config['Verify']['appuid'],
+        'ingress.intelmap.shflt': self.config['Local']['shflt'],
+        'ingress.intelmap.lat': self.config['Local']['lat'],
+        'ingress.intelmap.lng': self.config['Local']['lng'],
+        'ingress.intelmap.zoom': self.config['Local']['zoom'],
+        }
+        request = requests.get('https://www.ingress.com/intel', headers=headers, cookies=cookies, verify=False)
+        reg = '"(\w{5}-\w{37})"'
+        b = re.search(reg, request.text).group(1)
+        self.config['Verify']['b'] = b
 
